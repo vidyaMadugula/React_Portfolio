1d922f8d4ed60fe7aae651bdb5fdf850fefc6b58
vidyaMadugula <vidyamadugula692@gmail.com>
2024-03-23 14:00:45 +0530

    made changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white sm:pb-10 sm:pt-10 sm:h-full"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -62,6 +62,7 @@ const Portfolio = () => {
   
               </div>
               
+              
           </div>
             
             
